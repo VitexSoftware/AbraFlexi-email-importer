f7009627580e8d571336da20a2ec900fec22ab4b
CyberVitexus <info@vitexsoftware.cz>
2024-01-05 19:59:09 +0100

    MultiFlexi definition update

--- src/imap2abraflexi.php
+++ src/imap2abraflexi.php
@@ -4,7 +4,7 @@
  * Imap2AbraFlexi
  *
  * @author     Vítězslav Dvořák <info@vitexsofware.cz>
- * @copyright  (G) 2019-2023 Vitex Software
+ * @copyright  (G) 2019-2024 Vitex Software
  */
 
 namespace AbraFlexi\Imap2AF;
@@ -13,7 +13,7 @@ require_once '../vendor/autoload.php';
 
 define('EASE_APPNAME', 'Imap2AbraFlexi');
 
-\Ease\Shared::init(['ABRAFLEXI_URL', 'ABRAFLEXI_LOGIN', 'ABRAFLEXI_PASSWORD', 'ABRAFLEXI_COMPANY','ABRAFLEXI_BANK','ABRAFLEXI_STORAGE'], isset($argv[1]) ? $argv[1] : '../.env');
+\Ease\Shared::init(['ABRAFLEXI_URL', 'ABRAFLEXI_LOGIN', 'ABRAFLEXI_PASSWORD', 'ABRAFLEXI_COMPANY', 'ABRAFLEXI_BANK', 'ABRAFLEXI_STORAGE'], isset($argv[1]) ? $argv[1] : '../.env');
 \Ease\Locale::singleton('cs_CZ', '../i18n', 'imap2af');
 \Ease\Logger\Regent::singleton();
 
