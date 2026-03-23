e6ddcde027215c4d187f4c2a8f5fe1af664d7b35
CyberVitexus <info@vitexsoftware.cz>
2026-03-23 02:30:21 +0100

    deb autoloader update

--- debian/autoload.php
+++ debian/autoload.php
@@ -5,3 +5,8 @@ require_once '/usr/share/php/AbraFlexi/autoload.php';
 require_once '/usr/share/php/AbraFlexiBricks/autoload.php';
 require_once '/usr/share/php/PhpImap/autoload.php';
 require_once '/usr/share/php/Lightools/Xml/autoload.php';
+require_once '/usr/lib/abraflexi-email-importer/Imap2AF/Convertor.php';
+require_once '/usr/lib/abraflexi-email-importer/Imap2AF/Importer.php';
+require_once '/usr/lib/abraflexi-email-importer/Imap2AF/Mailboxer.php';
+require_once '/usr/lib/abraflexi-email-importer/Imap2AF/MailImporter.php';
+require_once '/usr/lib/abraflexi-email-importer/Imap2AF/Parser.php';
